4a3dd340b5d9d8bdae7ca1f6c6b7b150b85a2685
SIM Yuri <gr033@kyonggi.ac.kr>
2024-01-22 16:53:37 +0900

    day 11

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -4,3 +4,6 @@ INSERT INTO article(title, content) VALUES('3', '789');
 INSERT INTO member(id, email, password) VALUES(1, 'email@email.com', 'password123');
 INSERT INTO member(id, email, password) VALUES(2, 'email2@gmail.com', 'password23');
 INSERT INTO member(id, email, password) VALUES(3, 'emmail3@nate.com', 'password3');
+INSERT INTO coffee(name, price) VALUES('아메리카노', '4500');
+INSERT INTO coffee(name, price) VALUES('카페라떼', '5000');
+INSERT INTO coffee(name, price) VALUES('카페모카', '5500');
