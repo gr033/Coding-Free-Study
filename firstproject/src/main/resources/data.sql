8537f049643fd0926294797c6689f2348ae75b54
SIM Yuri <gr033@kyonggi.ac.kr>
2024-01-26 16:47:25 +0900

    day 14

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -7,3 +7,15 @@ INSERT INTO member(id, email, password) VALUES(3, 'emmail3@nate.com', 'password3
 INSERT INTO coffee(name, price) VALUES('아메리카노', '4500');
 INSERT INTO coffee(name, price) VALUES('카페라떼', '5000');
 INSERT INTO coffee(name, price) VALUES('카페모카', '5500');
+INSERT INTO article(title, content) VALUES('당신의 인생 영화는?', '댓글 고');
+INSERT INTO article(title, content) VALUES('당신의 소울 푸드는?', '댓글 고고');
+INSERT INTO article(title, content) VALUES('당신의 취미는?', '댓글 고고고');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Park', '굿 윌 헌팅');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Kim', '아메리칸 셰프');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'Choi', '쇼생크 탈출');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Park', '치킨');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Kim', '피자');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'Choi', '떡볶이');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Park', '조깅');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Kim', '유튜브 시청');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'Choi', '독서');
